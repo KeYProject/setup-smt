9434cbc1add8fe8eb3405da5194baec7f5c23097
Alexander Weigl <wadoon@users.noreply.github.com>
2025-02-27 16:33:50 +0000

    v0.2.0

--- dist/index.js
+++ dist/index.js
@@ -29820,6 +29820,9 @@ var coreExports = requireCore();
 // The operating system of the runner executing the job. Possible values are Linux, Windows, or macOS. For example, Windows
 const platform = (env.RUNNER_OS?.toLowerCase() || 'linux');
 async function download(tool, version, urls) {
+    if (!version) {
+        return;
+    }
     let toolPath = toolCacheExports.find(tool, version, platform);
     const info = urls[platform];
     if (info === undefined) {
@@ -29848,46 +29851,43 @@ async function download(tool, version, urls) {
 }
 async function run() {
     coreExports.debug(`Platform: ${platform}`);
-    await download('z3', '4.14.0', {
+    const z3Version = coreExports.getInput('z3Version');
+    await download('z3', z3Version, {
         linux: {
-            url: 'https://github.com/Z3Prover/z3/releases/download/z3-4.14.0/z3-4.14.0-x64-glibc-2.35.zip',
+            url: `https://github.com/Z3Prover/z3/releases/download/z3-${z3Version}/z3-${z3Version}-x64-glibc-2.35.zip`,
             format: 'zip',
-            binPath: 'z3-4.14.0-x64-glibc-2.35/bin/'
+            binPath: `z3-${z3Version}-x64-glibc-2.35/bin/`
         },
         windows: {
-            url: 'https://github.com/Z3Prover/z3/releases/download/z3-4.14.0/z3-4.14.0-x64-win.zip',
+            url: `https://github.com/Z3Prover/z3/releases/download/z3-${z3Version}/z3-${z3Version}-x64-win.zip`,
             format: 'zip',
-            binPath: 'z3-4.14.0-x64-win/bin/'
+            binPath: `z3-${z3Version}-x64-win/bin/`
         },
         macos: {
-            url: 'https://github.com/Z3Prover/z3/releases/download/z3-4.14.0/z3-4.14.0-x64-osx-13.7.2.zip',
+            url: `https://github.com/Z3Prover/z3/releases/download/z3-${z3Version}/z3-${z3Version}-x64-osx-13.7.2.zip`,
             format: 'zip',
-            binPath: 'z3-4.14.0-osx/bin/'
+            binPath: `z3-${z3Version}-osx/bin/`
         }
     });
-    await download('cvc5', '1.2.1', {
+    const cvc5Version = coreExports.getInput('cv5Version');
+    await download('cvc5', cvc5Version, {
         linux: {
-            url: 'https://github.com/cvc5/cvc5/releases/download/cvc5-1.2.1/cvc5-Linux-x86_64-static.zip',
+            url: `https://github.com/cvc5/cvc5/releases/download/cvc5-${cvc5Version}/cvc5-Linux-x86_64-static.zip`,
             format: 'zip',
-            binPath: 'cvc5-Linux-x86_64-static/bin/'
+            binPath: `cvc5-Linux-x86_64-static/bin/`
         },
         windows: {
-            url: 'https://github.com/cvc5/cvc5/releases/download/cvc5-1.2.1/cvc5-Win64-x86_64-static.zip',
+            url: `https://github.com/cvc5/cvc5/releases/download/cvc5-${cvc5Version}/cvc5-Win64-x86_64-static.zip`,
             format: 'zip',
-            binPath: 'cvc5-Win64-x86_64-static/bin/'
+            binPath: `cvc5-Win64-x86_64-static/bin/`
         },
         macos: {
-            url: 'https://github.com/cvc5/cvc5/releases/download/cvc5-1.2.1/cvc5-macOS-x86_64-static.zip',
+            url: `https://github.com/cvc5/cvc5/releases/download/cvc5-${cvc5Version}/cvc5-macOS-x86_64-static.zip`,
             format: 'zip',
-            binPath: 'cvc5-macOs-x86_64-static/bin/'
+            binPath: `cvc5-macOS-x86_64-static/bin/`
         }
     });
 }
 
-/**
- * The entrypoint for the action. This file simply imports and runs the action's
- * main logic.
- */
-/* istanbul ignore next */
 run();
 //# sourceMappingURL=index.js.map
---
+++ src/main.ts
@@ -0,0 +1,105 @@
+import * as tc from '@actions/tool-cache'
+import * as core from '@actions/core'
+import { env } from 'process'
+
+interface Tools {
+  linux: Tool | undefined
+  windows: Tool | undefined
+  macos: Tool | undefined
+}
+
+interface Tool {
+  url: string
+  format: string
+  binPath: string
+}
+
+type Platform = 'linux' | 'windows' | 'macos'
+
+// The operating system of the runner executing the job. Possible values are Linux, Windows, or macOS. For example, Windows
+const platform: Platform = (env.RUNNER_OS?.toLowerCase() || 'linux') as Platform
+
+async function download(
+  tool: string,
+  version: string,
+  urls: Tools
+): Promise<void> {
+  if (!version) {
+    return
+  }
+
+  let toolPath = tc.find(tool, version, platform)
+
+  const info: Tool | undefined = urls[platform]
+
+  if (info === undefined) {
+    return
+  }
+
+  if (!toolPath) {
+    const downloadPath = await tc.downloadTool(info.url)
+    let extractedPath: string
+
+    switch (info.format) {
+      case 'tar':
+        extractedPath = await tc.extractTar(downloadPath)
+        break
+      case '7z':
+        extractedPath = await tc.extract7z(downloadPath)
+        break
+      case 'xar':
+        extractedPath = await tc.extractXar(downloadPath)
+        break
+      default:
+        extractedPath = await tc.extractZip(downloadPath)
+    }
+
+    toolPath = await tc.cacheDir(extractedPath, tool, version, platform)
+  }
+
+  core.addPath(`${toolPath}/${info.binPath}`)
+  core.debug(`${toolPath}/${info.binPath} to PATH`)
+}
+
+export async function run(): Promise<void> {
+  core.debug(`Platform: ${platform}`)
+
+  const z3Version = core.getInput('z3Version')
+
+  await download('z3', z3Version, {
+    linux: {
+      url: `https://github.com/Z3Prover/z3/releases/download/z3-${z3Version}/z3-${z3Version}-x64-glibc-2.35.zip`,
+      format: 'zip',
+      binPath: `z3-${z3Version}-x64-glibc-2.35/bin/`
+    },
+    windows: {
+      url: `https://github.com/Z3Prover/z3/releases/download/z3-${z3Version}/z3-${z3Version}-x64-win.zip`,
+      format: 'zip',
+      binPath: `z3-${z3Version}-x64-win/bin/`
+    },
+    macos: {
+      url: `https://github.com/Z3Prover/z3/releases/download/z3-${z3Version}/z3-${z3Version}-x64-osx-13.7.2.zip`,
+      format: 'zip',
+      binPath: `z3-${z3Version}-osx/bin/`
+    }
+  })
+
+  const cvc5Version = core.getInput('cv5Version')
+  await download('cvc5', cvc5Version, {
+    linux: {
+      url: `https://github.com/cvc5/cvc5/releases/download/cvc5-${cvc5Version}/cvc5-Linux-x86_64-static.zip`,
+      format: 'zip',
+      binPath: `cvc5-Linux-x86_64-static/bin/`
+    },
+    windows: {
+      url: `https://github.com/cvc5/cvc5/releases/download/cvc5-${cvc5Version}/cvc5-Win64-x86_64-static.zip`,
+      format: 'zip',
+      binPath: `cvc5-Win64-x86_64-static/bin/`
+    },
+    macos: {
+      url: `https://github.com/cvc5/cvc5/releases/download/cvc5-${cvc5Version}/cvc5-macOS-x86_64-static.zip`,
+      format: 'zip',
+      binPath: `cvc5-macOS-x86_64-static/bin/`
+    }
+  })
+}
